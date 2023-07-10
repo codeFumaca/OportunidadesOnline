e9eeed223fb80d79f8fa111d3c7b4f11a406fd0f
codeFumaça <fakecrazzyy@hotmail.com>
2023-07-10 17:00:41 -0300

    index.js is now app.js

--- Back-end/scr/routes/routes.js
+++ Back-end/scr/routes/routes.js
@@ -79,4 +79,17 @@ router.put('/perfis/atualizar/:id', PerfilController.update);
 
 router.delete('/perfis/delete/:id', PerfilController.delete);
 
+// ROTAS PROJETO
+router.post('/register/perfil', PerfilController.store);
+
+router.get('/perfis', PerfilController.index);
+
+router.get('/perfis/search/id/:id', PerfilController.show);
+
+router.get('/perfis/search/nome/:nome', PerfilController.showbyname);
+
+router.put('/perfis/atualizar/:id', PerfilController.update);
+
+router.delete('/perfis/delete/:id', PerfilController.delete);
+
 export default router;
